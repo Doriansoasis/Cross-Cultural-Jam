42a4775b6994662555078ab63494b2ad7fd7be6e
Léonard Alarie <lelecopter@gmail.com>
2022-11-05 16:31:16 -0400

    Main menu and options started

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        Debug.Log("Game Quit");
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
